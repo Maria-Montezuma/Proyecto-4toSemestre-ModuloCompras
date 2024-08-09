2d30a267da12a9c30a68a3d42e00b0e9f0f840d3
maria montezuma <maria.30.montezuma@gmail.com>
2024-08-09 13:18:27 -0400

    vista de recepcion

--- app/Http/Controllers/RecepcionMercanciaController.php
+++ app/Http/Controllers/RecepcionMercanciaController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Empleado;
+use App\Models\Suministro;
 use App\Models\OrdenesCompra;
 use App\Models\DetallesRecepcionesMercancia;
 use Carbon\Carbon;
@@ -10,5 +12,21 @@ use App\Models\RecepcionesMercancia;
 
 class RecepcionMercanciaController extends Controller
 {
-    //
+    public function create()
+    {
+        $recepcionesMercancia = RecepcionesMercancia::with(['ordenes_compra.proveedore', 'empleado'])->orderBy('idRecepcion_mercancia', 'desc')->get();
+        $empleados = Empleado::all();
+        
+        // Obtener solo las órdenes de compra que no han sido recibidas
+        $ordenesCompra = OrdenesCompra::where('status', 2)
+            ->whereNotIn('idOrden_compra', function($query) {
+                $query->select('Ordenes_compras_idOrden_compra')
+                      ->from('recepciones_mercancias');
+            })
+            ->get();
+        
+        $suministros = Suministro::all(['idSuministro', 'nombre_suministro']);
+        
+        return view('recepcion', compact('empleados', 'ordenesCompra', 'suministros', 'recepcionesMercancia'));
+    }
 }
